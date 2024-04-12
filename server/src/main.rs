52a8f77befd1a4bca3453f28387f278453d6d38e
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2024-04-12 23:33:18 +0200

    serve static files

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    actix_web::HttpServer::new(|| {
+        actix_web::App::new()
+            .service(actix_files::Files::new("/", "./dist").index_file("index.html"))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
