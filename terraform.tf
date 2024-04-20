2b404278159f0b758e5bd85d6ed9f2559678673b
CrazyCraftix <43807375+CrazyCraftix@users.noreply.github.com>
2024-04-20 18:25:01 +0200

    ansible setup

--- terraform.tf
+++ terraform.tf
@@ -16,12 +16,24 @@ provider "google" {
   zone    = "europe-west1-b"
 }
 
-resource "google_compute_instance" "webserver" {
-  name         = "webserver"
+resource "google_compute_firewall" "web-traffic" {
+  name    = "web-traffic"
+  target_tags = ["web"]
+  network = "default"
+  source_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+}
+
+resource "google_compute_instance" "webserver-1" {
+  name         = "webserver-1"
   machine_type = "e2-micro"
   metadata = {
     ssh-keys = "user:${file(var.ssh_public_key_file_path)}"
   }
+  tags = ["web"]
 
   boot_disk {
     initialize_params {
@@ -35,6 +47,10 @@ resource "google_compute_instance" "webserver" {
   }
 }
 
-output "webserver_ip" {
-  value = google_compute_instance.webserver.network_interface.0.access_config.0.nat_ip
+resource "local_file" "ansible-node-inventory" {
+    filename = "./ansible-node-inventory"
+    content     = <<-EOF
+    [webservers]
+    ${google_compute_instance.webserver-1.network_interface.0.access_config.0.nat_ip}
+    EOF
 }
